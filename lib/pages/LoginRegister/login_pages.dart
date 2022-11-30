2a5646e26285ad2d5e5f8ccf14776c7b4535c9eb
khrisnandika <khrisnandika15@gmail.com>
2022-11-30 14:03:53 +0700

    backend login firebase

--- lib/pages/LoginRegister/login_pages.dart
+++ lib/pages/LoginRegister/login_pages.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:healthcare/core/const.dart';
@@ -5,6 +7,8 @@ import 'package:healthcare/pages/LoginRegister/register_pages.dart';
 import 'package:healthcare/pages/NavigatorBar/navbar.dart';
 import 'package:healthcare/widgets/animation.dart';
 import 'package:get/get.dart';
+import '../../models/ChatModels/UIHelper.dart';
+import '../../models/ChatModels/UserModel.dart';
 import '../../models/login_register_password_controller.dart';
 
 void main() => runApp(MaterialApp(
@@ -12,222 +16,264 @@ void main() => runApp(MaterialApp(
       home: LoginPage(),
     ));
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final passCtrl = Get.put(passwordController());
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void checkValues() {
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+
+    if (email == "" || password == "") {
+      UIHelper.showAlertDialog(
+          context, "Incomplete Data", "Please fill all the fields");
+    } else {
+      logIn(email, password);
+    }
+  }
+
+  void logIn(String email, String password) async {
+    UserCredential? credential;
+
+    UIHelper.showLoadingDialog(context, "Logging In..");
+
+    try {
+      credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (ex) {
+      // Close the loading dialog
+      Navigator.pop(context);
+
+      // Show Alert Dialog
+      UIHelper.showAlertDialog(
+          context, "An error occured", ex.message.toString());
+    }
+
+    if (credential != null) {
+      String uid = credential.user!.uid;
+
+      DocumentSnapshot userData =
+          await FirebaseFirestore.instance.collection('users').doc(uid).get();
+      UserModel userModel =
+          UserModel.fromMap(userData.data() as Map<String, dynamic>);
+
+      // Go to HomePage
+      print("Log In Successful!");
+      Navigator.popUntil(context, (route) => route.isFirst);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) {
+          return NavigasiBar();
+        }),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 400,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/image/background2.png'),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned(
-                        right: 50,
-                        top: 25,
-                        width: 300,
-                        height: 200,
-                        child: FadeAnimation(
-                          1.5,
-                          Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage('assets/image/logoWhite.png'),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      // Positioned(
-                      //   child: FadeAnimation(
-                      //     1.6,
-                      //     Container(
-                      //       margin: EdgeInsets.only(top: 50),
-                      //       child: Center(
-                      //         child: Text(
-                      //           "Selamat Datang",
-                      //           style: TextStyle(
-                      //               color: Colors.white,
-                      //               fontSize: 28,
-                      //               fontWeight: FontWeight.bold),
-                      //         ),
-                      //       ),
-                      //     ),
-                      //   ),
-                      // ),
-                      Positioned(
-                        child: FadeAnimation(
-                          1.6,
-                          Container(
-                            margin: EdgeInsets.only(top: 350),
-                            child: Center(
-                              child: Text(
-                                "Masuk",
-                                style: GoogleFonts.inter(
-                                  color: kHealthCareColor,
-                                  fontSize: 29,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/image/background2.png'),
+                    fit: BoxFit.fill,
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(30.0),
-                  child: Column(
-                    children: <Widget>[
-                      FadeAnimation(
-                          1.8,
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Color.fromRGBO(143, 148, 251, .2),
-                                  blurRadius: 20.0,
-                                  offset: Offset(0, 10),
-                                ),
-                              ],
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      right: 50,
+                      top: 25,
+                      width: 300,
+                      height: 200,
+                      child: FadeAnimation(
+                        1.5,
+                        Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/image/logoWhite.png'),
                             ),
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(8.0),
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom:
-                                              BorderSide(color: Colors.grey))),
-                                  child: TextFormField(
-                                    decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "Username atau Email",
-                                        hintStyle:
-                                            TextStyle(color: Colors.grey[400])),
-                                  ),
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Obx(
-                                    () => TextFormField(
-                                      obscureText: passCtrl.showPassword.value,
-                                      decoration: InputDecoration(
-                                        suffixIcon: GestureDetector(
-                                          onTap: () {
-                                            passCtrl.showPass();
-                                          },
-                                          child: Icon(
-                                            passCtrl.showPassword.value
-                                                ? Icons.visibility
-                                                : Icons.visibility_off,
-                                            color: kGreyColor.withOpacity(0.5),
-                                          ),
-                                        ),
-                                        border: InputBorder.none,
-                                        hintText: "Password",
-                                        hintStyle: TextStyle(
-                                          color: Colors.grey[400],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )),
-                      SizedBox(
-                        height: 30,
+                          ),
+                        ),
                       ),
-                      FadeAnimation(
-                        2,
-                        GestureDetector(
-                          onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => NavigasiBar(),
-                              )),
-                          child: Container(
-                            height: 50,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                gradient: LinearGradient(colors: [
-                                  Color.fromRGBO(10, 184, 133, 1.000),
-                                  Color.fromRGBO(10, 184, 133, .600),
-                                ])),
-                            child: Center(
-                              child: Text(
-                                "Masuk",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                    ),
+                    Positioned(
+                      child: FadeAnimation(
+                        1.6,
+                        Container(
+                          margin: EdgeInsets.only(top: 350),
+                          child: Center(
+                            child: Text(
+                              "Masuk",
+                              style: GoogleFonts.inter(
+                                color: kHealthCareColor,
+                                fontSize: 29,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      FadeAnimation(
-                        1.5,
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children: <Widget>[
+                    FadeAnimation(
+                        1.8,
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(143, 148, 251, .2),
+                                blurRadius: 20.0,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
                             children: <Widget>[
                               Container(
-                                child: Text(
-                                  "Tidak Memiliki Akun?",
-                                  style: TextStyle(
-                                    color: kGreyColor,
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(color: Colors.grey),
                                   ),
                                 ),
+                                child: TextFormField(
+                                  controller: _emailController,
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Email",
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[400])),
+                                ),
                               ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => RegisterPage(),
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                child: Obx(
+                                  () => TextFormField(
+                                    controller: _passwordController,
+                                    obscureText: passCtrl.showPassword.value,
+                                    decoration: InputDecoration(
+                                      suffixIcon: GestureDetector(
+                                        onTap: () {
+                                          passCtrl.showPass();
+                                        },
+                                        child: Icon(
+                                          passCtrl.showPassword.value
+                                              ? Icons.visibility
+                                              : Icons.visibility_off,
+                                          color: kGreyColor.withOpacity(0.5),
+                                        ),
+                                      ),
+                                      border: InputBorder.none,
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(
+                                        color: Colors.grey[400],
+                                      ),
                                     ),
-                                  );
-                                },
-                                child: Text(
-                                  "Buat Akun",
-                                  style: TextStyle(
-                                    color: kHealthCareColor,
                                   ),
                                 ),
                               ),
                             ],
                           ),
+                        )),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    FadeAnimation(
+                      2,
+                      GestureDetector(
+                        onTap: () {
+                          checkValues();
+                        },
+                        child: Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              gradient: LinearGradient(colors: [
+                                Color.fromRGBO(10, 184, 133, 1.000),
+                                Color.fromRGBO(10, 184, 133, .600),
+                              ])),
+                          child: Center(
+                            child: Text(
+                              "Masuk",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    FadeAnimation(
+                      1.5,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                "Tidak Memiliki Akun?",
+                                style: TextStyle(
+                                  color: kGreyColor,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => RegisterPage(),
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                "Buat Akun",
+                                style: TextStyle(
+                                  color: kHealthCareColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
