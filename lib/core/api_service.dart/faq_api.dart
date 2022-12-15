b50b66a638d2493e322c7f04be5a4baa800d01ac
khrisnandika <khrisnandika15@gmail.com>
2022-12-16 00:55:37 +0700

    adding api service

---
+++ lib/core/api_service.dart/faq_api.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:healthcare/models/ApiModels/faq.dart';
+import 'package:http/http.dart' as http;
+
+class Repository {
+  final _baseUrl = 'http://10.0.2.2/healthcare-admin/api/getFaq.php';
+
+  Future getData() async {
+    try {
+      final response = await http.get(
+        Uri.parse(_baseUrl),
+      );
+      if (response.statusCode == 200) {
+        print(response.body);
+        Iterable it = jsonDecode(response.body);
+        List<Faq> album = it.map((e) => Faq.fromJson(e)).toList();
+        return album;
+      }
+    } catch (e) {}
+  }
+}
---
+++ lib/core/api_service.dart/perawat_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:healthcare/models/ApiModels/faq.dart';
+import 'package:healthcare/models/ApiModels/perawat.dart';
+import 'package:http/http.dart' as http;
+
+class RepoPerawat {
+  final _baseUrl = 'http://10.0.2.2/healthcare-admin/api/listPerawat.php';
+
+  Future getData() async {
+    try {
+      final response = await http.get(
+        Uri.parse(_baseUrl),
+      );
+      if (response.statusCode == 200) {
+        print(response.body);
+        Iterable it = jsonDecode(response.body);
+        List<PerawatApi> perawat_api = it.map((e) => PerawatApi.fromJson(e)).toList();
+        return perawat_api;
+      }
+    } catch (e) {}
+  }
+}
