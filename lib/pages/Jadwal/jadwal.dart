4b405fbeefb7ab3374460e2e77b7351791810dd7
khrisnandika <khrisnandika15@gmail.com>
2022-12-17 03:22:52 +0700

    adding schedule page

---
+++ lib/pages/Jadwal/jadwal.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:healthcare/core/const.dart';
+
+class JadwalPage extends StatefulWidget {
+  const JadwalPage({super.key});
+
+  @override
+  State<JadwalPage> createState() => _JadwalPageState();
+}
+
+class _JadwalPageState extends State<JadwalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+    );
+  }
+}
